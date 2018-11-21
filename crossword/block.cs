6927f3f285fd688ad2581b408837f0383cc3bc3f
DrastiKon <45232523+DrastiKon@users.noreply.github.com>
2018-11-21 16:14:40 +0200

    Add Block.cs

---
+++ crossword/block.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crossword
+{
+    public enum BlockState
+    {
+        Black ,
+        Empty ,
+        Confirmed ,
+        Unconfirmed 
+    };
+
+    
+
+    class Block
+    {
+        private BlockState state;
+        private char character;
+        
+        public BlockState GetBlockState()
+        {
+            return state;
+        }
+
+
+        bool IsBlack()
+        {
+            if (state == BlockState.Black)
+            {
+                return true;
+            }
+            return false;
+        }
+        bool IsEmpty()
+        {
+            if (state == BlockState.Empty)
+            {
+                return true;
+            }
+            return false;
+        }
+        bool IsConfirmed()
+        {
+            if (state == BlockState.Confirmed)
+            {
+                return true;
+            }
+            return false;
+        }
+        bool IsUnconfirmed()
+        {
+            if (state == BlockState.Unconfirmed)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        bool CanPlaceCharacer(char charact)
+        {
+            return IsEmpty() || IsUnconfirmed();
+        }
+
+        void PlaceCharacter(char charact)
+        {
+            if (CanPlaceCharacer(charact))
+            {
+                character = charact;
+            }
+        }
+        void ConfirmCharacter()
+        {
+            state = BlockState.Confirmed;
+        }
+        
+    }
+}
