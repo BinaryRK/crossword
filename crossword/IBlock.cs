e9c1642128043f3ccd809dcf21b9f97cfa30a6ba
BinaryRK <fox.dealos@gmail.com>
2018-11-22 14:54:06 +0200

    Add files for new classes

---
+++ crossword/IBlock.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace crossword
+{
+    interface IBlock
+    {
+        bool IsCorrectAnswer();
+
+        bool IsPartOfVerticalWord();
+        Word GetVerticalWord();
+
+        bool IsPartOfHorizontalWord();
+        Word GetHorizontalWord();
+
+        Control GetVisualControl();
+
+        void SetCharacter(char c);
+
+        // Attempts to validate the current character
+        // returns true if successful
+        bool TryValidate();
+    }
+}
